3e4333fc8d5310692a7306cb848f06a2d9479ade
Violetz <m001747@permatapintar.ukm.edu.my>
2024-12-13 16:06:20 +0800

    commit

---
+++ login_process.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // CHANGE BASED ON YOUR WEBSITE
+    if ($username === "Zandy" && $password === "123456") 
+    { 
+        
+        echo "Login successful!";
+        header("Location: login sucess.html");
+    } else {
+        
+        echo "Invalid username or password.";
+    }
+} else {
+    
+    header("Location: index.html");
+    exit();
+}
+?>
