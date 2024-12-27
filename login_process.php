8fc873a0ba696dd05bb85a8f6c82c405a4c69f54
codingkatty <m001720@permatapintar.ukm.edu.my>
2024-12-27 20:19:20 +0800

    linkss

--- login_process.php
+++ login_process.php
@@ -3,20 +3,16 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // CHANGE BASED ON YOUR WEBSITE
     if ($username === "Zandy" && $password === "123456") 
     { 
-        
         echo "Login successful!";
-        header("Location: login sucess.html");
+        header("Location: https://violetz12.github.io/Partner-Portfolio/login_sucess");
     } else {
-        
         echo "Invalid username or password.";
-        header("Location: login fail.html");
+        header("Location: https://violetz12.github.io/Partner-Portfolio/login_fail");
     }
 } else {
-    
-    header("Location: index.html");
+    header("Location: https://violetz12.github.io/Partner-Portfolio");
     exit();
 }
 ?>
